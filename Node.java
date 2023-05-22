d65cd498efac405dcb547377e24cb476db02f341
Excelim <127103742+Excelim@users.noreply.github.com>
2023-05-22 17:43:21 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.modul5sd;
+
+/**
+ *
+ * @author EXCEL
+ */
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data) {
+        this.data = data;
+    }
+}
